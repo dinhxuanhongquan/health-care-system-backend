60d0e1790246d1f3269cccc442c638914faf774e
dinhxuanhongquan <quandinh.09022003@gmail.com>
2025-07-20 16:17:34 +0700

    fix: edit config

--- src/main/java/com/example/HealthCareSystem/Configuration/CustomJwtDecoder.java
+++ src/main/java/com/example/HealthCareSystem/Configuration/CustomJwtDecoder.java
@@ -1,16 +1,24 @@
 package com.example.HealthCareSystem.Configuration;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomJwtDecoder implements JwtDecoder {
+
     @Value("${jwt.signerKey}")
-    private String signerKey;
+    protected String signerKey;
 
 
     public CustomJwtDecoder() {
-        this.
+
+    }
+
+    @Override
+    public Jwt decode(String token) throws JwtException {
+        return null;
     }
 }
--- src/main/java/com/example/HealthCareSystem/Configuration/JwtProperties.java
+++ src/main/java/com/example/HealthCareSystem/Configuration/JwtProperties.java
@@ -1,4 +1,16 @@
 package com.example.HealthCareSystem.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
 public class JwtProperties {
+    private String signerKey;
+    private long validDuration;
+    private long refreshableDuration;
 }
