6c8d2f6ab70c645f02f6302c64b9aae4f0dfd181
dinhxuanhongquan <quandinh.09022003@gmail.com>
2025-07-21 01:38:23 +0700

    fix: updated PrescriptionResponse

--- src/main/java/com/example/HealthCareSystem/Dto/Response/PrescriptionRes.java
+++ src/main/java/com/example/HealthCareSystem/Dto/Response/PrescriptionRes.java
@@ -1,8 +1,16 @@
 package com.example.HealthCareSystem.Dto.Response;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class PrescriptionRes {
     String prescriptionId;
 
@@ -13,6 +21,7 @@ public class PrescriptionRes {
     LocalDateTime expiryDate;
     LocalDateTime createdAt;
     LocalDateTime updatedAt;
+
     PatientRes patient; // Reference to the associated patient
     DoctorRes doctor; // Reference to the associated doctor
     MedicalRecordsRes medicalRecord; // Reference to the associated medical record
