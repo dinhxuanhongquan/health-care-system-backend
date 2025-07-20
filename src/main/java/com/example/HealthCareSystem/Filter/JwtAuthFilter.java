fdc1420744e34a5258c05a76f10a53f143643117
dinhxuanhongquan <quandinh.09022003@gmail.com>
2025-07-20 16:13:58 +0700

    feat: create new Filter

--- src/main/java/com/example/HealthCareSystem/Filter/JwtAuthFilter.java
+++ src/main/java/com/example/HealthCareSystem/Filter/JwtAuthFilter.java
@@ -1,4 +1,36 @@
 package com.example.HealthCareSystem.Filter;
 
-public class JwtAuthFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+
+    String extractToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if("JWT_TOKEN".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
 }
