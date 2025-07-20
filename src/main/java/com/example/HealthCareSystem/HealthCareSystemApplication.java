0ed905f759cbc35f175bfac8746cea8273caf982
dinhxuanhongquan <quandinh.09022003@gmail.com>
2025-07-20 16:11:07 +0700

    fix: add Annotation

--- src/main/java/com/example/HealthCareSystem/HealthCareSystemApplication.java
+++ src/main/java/com/example/HealthCareSystem/HealthCareSystemApplication.java
@@ -1,9 +1,12 @@
 package com.example.HealthCareSystem;
 
+import com.example.HealthCareSystem.Configuration.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class HealthCareSystemApplication {
 
 	public static void main(String[] args) {
