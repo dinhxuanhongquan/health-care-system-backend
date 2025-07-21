0b3af07d04e91f442125482a8e5fa70bfcf3aa1f
dinhxuanhongquan <quandinh.09022003@gmail.com>
2025-07-21 23:43:35 +0700

    feat: create new Request

---
+++ src/main/java/com/example/HealthCareSystem/Dto/Request/LogoutReq.java
@@ -0,0 +1,13 @@
+package com.example.HealthCareSystem.Dto.Request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LogoutReq {
+    String token;
+}
