420159bc3a5a0595b539e2c43fdd711e139fc39d
dinhxuanhongquan <quandinh.09022003@gmail.com>
2025-07-25 00:24:55 +0700

    fix: add error definition

--- src/main/java/com/example/HealthCareSystem/Exception/ErrorCode.java
+++ src/main/java/com/example/HealthCareSystem/Exception/ErrorCode.java
@@ -12,6 +12,8 @@ public enum ErrorCode {
 
     INVALID_KEY(1003, "Invalid key provided", HttpStatus.BAD_REQUEST),
     INVALID_DOB(1004, "Invalid date of birth provided", HttpStatus.BAD_REQUEST),
+    INVALID_TOKEN(1004, "Invalid token provided", HttpStatus.UNAUTHORIZED),
+    INVALID_CREDENTIALS(1009, "Invalid credentials provided", HttpStatus.UNAUTHORIZED),
 
     USER_EXISTED(1005, "User already exists", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1006, "User does not exist", HttpStatus.NOT_FOUND),
@@ -20,6 +22,14 @@ public enum ErrorCode {
     PASSWORD_VALIDATION(1008, "Password must be at least {min} characters long", HttpStatus.BAD_REQUEST),
 
     UNCATEGORIZED_EXCEPTION(9991, "Uncategorized Error", HttpStatus.INTERNAL_SERVER_ERROR),
+    TOKEN_GENERATION_FAILED(9992, "Token generation failed", HttpStatus.INTERNAL_SERVER_ERROR),
+
+    ROLE_NOT_EXISTED(2000, "Role does not exist", HttpStatus.NOT_FOUND),
+
+    USER_NOT_CREATED(2001, "User could not be created", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_UPDATED(2002, "User could not be updated", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_DELETED(2003, "User could not be deleted", HttpStatus.INTERNAL_SERVER_ERROR),
+
 
     ;
     ErrorCode(int code, String message, HttpStatusCode statusCode){
---
+++ src/main/java/com/example/HealthCareSystem/Service/RoleService.java
@@ -0,0 +1,4 @@
+package com.example.HealthCareSystem.Service;
+
+public class RoleService {
+}
